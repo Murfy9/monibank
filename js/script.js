7c75b03e987691443d3f898b3abeb38cb311134b
Pedro Xavier <pedroxxavier@hotmail.com>
2023-04-24 14:36:05 -0300

    Verificar CPF

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
+import ehUmCPF from "./valida-cpf.js";
 const campoDoFormulario = document.querySelectorAll("[required]");
 
 campoDoFormulario.forEach((campo) => {
   campo.addEventListener("blur", () => verificaCampo(campo));
 });
 
-function verificaCampo(campo) {}
+function verificaCampo(campo) {
+  if (campo.name == "cpf" && campo.value.length >= 11) {
+    ehUmCPF(campo);
+  }
+}
